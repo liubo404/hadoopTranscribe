5f97e0cae99c373b8ec80f13bd56b372263ff675
liubo404 <liubo404@gmail.com>
2014-07-01 13:54:22 +0800

    begin transcribe ContainerManagementProtocol

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/api/ContainerManagementProtocol.java
@@ -0,0 +1,7 @@
+package org.apache.hadoop.yarn.api;
+
+import java.io.IOException;
+
+import org.apache.hadoop.classification.InterfaceAudience.Public;
+import org.apache.hadoop.classification.InterfaceAudience.Stable;
+import org.apache.hadoop.yarn.api.protocolrecords.GetContainerStatusesRequest;
