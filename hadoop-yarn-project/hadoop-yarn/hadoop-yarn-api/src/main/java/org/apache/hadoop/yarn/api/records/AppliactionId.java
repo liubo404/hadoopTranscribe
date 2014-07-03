cd9068cf93c685006267d938cef046747203b3c0
liubo404 <liubo404@gmail.com>
2014-07-03 09:07:57 +0800

    ApplicationId.java half completed

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/api/records/AppliactionId.java
@@ -0,0 +1,72 @@
+package org.apache.hadoop.yarn.api.records;
+
+import java.text.NumberFormat;
+
+import org.apache.hadoop.classification.InterfaceAudience.Private;
+import org.apache.hadoop.classification.InterfaceAudience.Public;
+import org.apache.hadoop.classification.InterfaceStability.Stable;
+import org.apache.hadoop.classification.InterfaceStability.Unstable;
+import org.apache.hadoop.yarn.util.Records;
+
+@Public
+@Stable
+public abstract class Applicationid implements Comparable<ApplicationId> {
+
+    @Private
+        @Unstable
+        public static final String appIdStrPrefix = "application_";
+    @Private
+        @Unstable
+        public static ApplicationId newInstance(long clusterTimestamp, int id){
+        ApplicationId appId = Records.newRecord(ApplicationId.class);
+        appId.setClusterTimestamp(clusterTimestamp);
+        appId.setId(id);
+        appId.build();
+        return appId;
+    }
+
+    @Public
+        @Stable
+        public abstract int getId();
+
+    @Private
+        @Unstable
+        protected abstract void setId(int id);
+
+    @Public
+        @Stable
+        public abstract long getClusterTimestamp();
+
+    @Private
+        @Unstable
+        protected abstract void setClusterTimestamp(long clusterTimestamp);
+
+    protected abstract void build();
+
+    static final ThreadLocal<NumberFormat> appIdFormat =
+        new ThreadLocal<NumberFormat>(){
+          @Override
+          public NumberFormat initialValue() {
+              NumberFormat fmt = NumberFormat.getInstance();
+              fmt.setGroupingUsed(false);
+              fmt.setMinimumIntegerDigits(4);
+              return fmt;
+          }
+    };
+
+    @Override
+    public int compareTo(ApplicationId other) {
+        if(this.getClusterTimestampt() - other.getClusterTimestamp() == 0) {
+            return this.getId() - other.getId();
+        } else {
+            return this.getClusterTimestamp() > othre.getClusterTimestamp() ? 1 :
+                this.getClusterTimestamp() < other.getClusterTimestamp() ? -1 :0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return appIdStrPrefix + this.getClusterTimestamp() + "_"
+            + appIdFormat.get().format(getId());
+    }
+}
